2045837bccb6d4a091676910f88d55c1e07d0c77
LucVasSou <lucasbmbr@hotmail.com>
2021-07-24 17:07:46 -0300

    projeto v3

--- cadastro.js
+++ cadastro.js
@@ -1,10 +1,31 @@
 formjs.addEventListener('click', e => {
   e.preventDefault()
-  console.log(e)
+
+  let nome = document.getElementById('name').value
+  console.log(nome)
+  let data = document.getElementById('data').value
+  console.log(data)
+  let genero = document.getElementsByName('genero').value
+  console.log(genero)
+  let telefone = document.getElementById('telefone').value
+  console.log(telefone)
   let email = document.getElementById('preencher').value
   console.log(email)
+  let cidade = document.getElementById('cidade').value
+  console.log(cidade)
+  let estado = document.getElementById('estado').value
+  console.log(estado)
+  let endereco = document.getElementById('endereco').value
+  console.log(endereco)
   let data = {
-    email
+    nome,
+    data,
+    genero,
+    telefone,
+    email,
+    cidade,
+    estado,
+    endereco
   }
   let convertData = JSON.stringify(data)
 
